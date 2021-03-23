431b5843341741c16a16371f42f4a564783f8f8a
Samuel Pavlík <pavlik.samuel@gmail.com>
2021-03-23 12:05:55 +0100

    MenuHero chooses enemy with closest distance

--- Source/TopDownShooter/MenuHeroController.cpp
+++ Source/TopDownShooter/MenuHeroController.cpp
@@ -8,8 +8,18 @@
 void AMenuHeroController::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
-	if (MenuHero->GetTargets().Num() > 0)
-		SetFocus(MenuHero->GetTargets()[0]);
+	if (MenuHero->GetTargets().Num() > 0) {
+		AActor* Closest = MenuHero->GetTargets()[0];
+		float ClosDist = FVector::Distance(Closest->GetActorLocation(), MenuHero->GetActorLocation());
+		for (AActor* Current : MenuHero->GetTargets()) {
+			float CurrDist = FVector::Distance(Current->GetActorLocation(), MenuHero->GetActorLocation());
+			if (CurrDist < ClosDist) {
+				Closest = Current;
+				ClosDist = CurrDist;
+			}
+		}
+		SetFocus(Closest);
+	}
 
 	if (MenuHero->NeedsToReload())
 		MenuHero->ReloadWeapon();
