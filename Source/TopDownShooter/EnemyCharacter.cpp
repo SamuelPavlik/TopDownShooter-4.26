15ea827ca0d937ab06d1d495e2a3def2062e975c
Samuel Pavlík <pavlik.samuel@gmail.com>
2021-08-04 12:01:09 +0200

    Zombie Build move speed slower than debug move speed fixed
    Added delta seconds to calculation for enemy move

--- Source/TopDownShooter/EnemyCharacter.cpp
+++ Source/TopDownShooter/EnemyCharacter.cpp
@@ -24,6 +24,7 @@ void AEnemyCharacter::Kill() {
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldDynamic, ECollisionResponse::ECR_Ignore);
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
 	DetachFromControllerPendingDestroy();
+	//stop attack montage if present
 	StopAnimMontage();
 
 	FTimerHandle Handle;
